d85717ef85b644f195e795d85a2d3c4902135180
ValdemarPM <valdemar.matos@gmail.com>
2024-05-14 19:56:14 +0200

    SVGs & Emojis over image
    SVG commented for now, because it's more complicated to work with than PNGs

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,52 @@
-import 'package:cooszy_pre/screens/bases/persisten_bottom_nav_bar_demo.dart';
-import 'package:cooszy_pre/themes/theme_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:cooszy_pre/screens/bases/assets_utils.dart';
+import 'package:cooszy_pre/screens/bases/persisten_bottom_nav_bar_demo.dart';
+import 'package:cooszy_pre/themes/theme_provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(ChangeNotifierProvider(
     create: (context) => ThemeProvider(),
     child: const MyApp(),
   ));
 }
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _precacheAssets(context);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cooszy',
+      theme: Provider.of<ThemeProvider>(context).themeData,
+      home: const PersistenBottomNavBarDemo(),
+    );
+  }
+}
+
+Future<void> _precacheAssets(BuildContext context) async {
+  // Skipping SVGs for now.
+  //await AssetsUtils.svgPrecacheImages(context);
+  // ignore: use_build_context_synchronously
+  await AssetsUtils.imagePrecache(context);
+}
+
+/* ---
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -24,3 +62,11 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+Future<void> _precacheAssets(BuildContext context) async {
+  await AssetsUtils.svgPrecacheImages(context);
+  await AssetsUtils.imagePrecache(context);
+}
+
+
+*/
--- lib/screens/pages/calendar.dart
+++ lib/screens/pages/calendar.dart
@@ -1,14 +1,41 @@
+import 'package:cooszy_pre/screens/bases/assets_utils.dart';
 import 'package:cooszy_pre/screens/bases/base_widget.dart';
 import 'package:flutter/material.dart';
+//import 'package:flutter_svg/svg.dart';
 
 class Calendar extends StatefulWidget {
   const Calendar({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
   _CalendarState createState() => _CalendarState();
 }
 
 class _CalendarState extends State<Calendar> {
+  /*
+  final String emojiIntimateIcon = AssetsUtils.emojiIntimate;
+  final String emojiCloseIcon = AssetsUtils.emojiClose;
+  Widget svgIntimate = Container();
+  Widget svgClose = Container();
+
+  @override
+  void initState() {
+    super.initState();
+    svgIntimate = SvgPicture.asset(
+      emojiIntimateIcon,
+      semanticsLabel: "Intimate",
+      width: 40,
+      height: 40,
+    );
+    svgClose = SvgPicture.asset(
+      emojiCloseIcon,
+      semanticsLabel: "Close",
+      width: 40,
+      height: 40,
+    );
+  }
+  */
+
   @override
   Widget build(BuildContext context) {
     return BaseWidget(
@@ -19,8 +46,44 @@ class _CalendarState extends State<Calendar> {
         appBar: AppBar(
           title: const Text("Calendar"),
         ),
-        body: const Center(
-          child: Text("Calendar"),
+        body: Column(
+          children: [
+            /*
+            Container(
+              decoration: BoxDecoration(boxShadow: [
+                BoxShadow(
+                    color: Theme.of(context).colorScheme.onInverseSurface,
+                    spreadRadius: 1,
+                    blurRadius: 3,
+                    offset: const Offset(-1, 4))
+              ], borderRadius: const BorderRadius.all(Radius.circular(20))),
+              child: svgClose, // Use the svg here
+            ),
+            Center(
+              child: svgIntimate, // Use the svg here
+            ),
+            */
+            Center(
+                child: Image.asset(
+              AssetsUtils.emojiLevelNasty,
+              width: 40,
+              height: 40,
+            )),
+            Container(
+              decoration: BoxDecoration(boxShadow: [
+                BoxShadow(
+                    color: Theme.of(context).colorScheme.onInverseSurface,
+                    spreadRadius: 1,
+                    blurRadius: 3,
+                    offset: const Offset(-1, 4))
+              ], borderRadius: const BorderRadius.all(Radius.circular(20))),
+              child: Image.asset(
+                AssetsUtils.emojiLevelCasual,
+                width: 40,
+                height: 40,
+              ),
+            )
+          ],
         ),
       ),
     );
